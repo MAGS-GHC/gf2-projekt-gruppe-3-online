2c2d8574d7962f2600f72fe6dda5f69e4c4d2aff
EDMerstratec <erme0001@edu.mercantec.dk>
2023-11-27 11:59:53 +0100

    EDM init planning

---
+++ PersonalMaps/EDM/VisualizeData/visualize.js
@@ -0,0 +1,15 @@
+const KPItest1 = {
+    orders: 11,
+    precision: 25.00,
+    accidents: 1
+}
+const KPItest2 = {
+    orders: 12,
+    precision: 50.00,
+    accidents: 2
+}
+const KPItest3 = {
+    orders: 13,
+    precision: 75.00,
+    accidents: 3
+}
