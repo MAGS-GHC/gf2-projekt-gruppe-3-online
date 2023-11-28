d91927b9f5b80738e5dba520d79dd5ec95cfe44f
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-11-28 19:10:21 +0100

    D. 28/11 kl. 19.10

--- PersonalMaps/Mikkel/Mikkel Banko/Banko.js
+++ PersonalMaps/Mikkel/Mikkel Banko/Banko.js
@@ -1,4 +1,50 @@
-function bankoRow1() {
+// Counts Empty fields functions
+function countEmptyFields() {
+    let emptyFieldCount = 0;
+
+    for (let i = 0; i <= 8; i++) {
+        let elementId = "r0c" + i;
+        let fieldValue = document.getElementById(elementId).value;
+
+        if (fieldValue === "") {
+            emptyFieldCount++;
+        }
+    }
+
+    return emptyFieldCount;
+}
+
+function countEmptyFields1() {
+    let emptyFieldCount = 0;
+
+    for (let i = 0; i <= 8; i++) {
+        let elementId = "r1c" + i;
+        let fieldValue = document.getElementById(elementId).value;
+
+        if (fieldValue === "") {
+            emptyFieldCount++;
+        }
+    }
+
+    return emptyFieldCount;
+}
+
+function countEmptyFields2() {
+    let emptyFieldCount = 0;
+
+    for (let i = 0; i <= 8; i++) {
+        let elementId = "r2c" + i;
+        let fieldValue = document.getElementById(elementId).value;
+
+        if (fieldValue === "") {
+            emptyFieldCount++;
+        }
+}
+
+    return emptyFieldCount;
+}
+    
+function bankoRow() {
 
 let loopOnOff = 0;
 
@@ -16,9 +62,11 @@ do {
     for (let i = 0; i < 5; i++) {
         let randomNumber;
 
+
+        // udtager random tal og tjekker at alle numre er unikke
         do {
             randomNumber = Math.floor(Math.random() * 90) + 1;
-        } while (randomNumbersArray.includes(randomNumber));
+        } while (randomNumbersArray.includes(randomNumber)); //includes er sand hvis random number findes i arrayet
 
         randomNumbersArray.push(randomNumber);
     }
@@ -31,16 +79,16 @@ do {
         let low = i * 10;
         let high = low + 9
     
+        if (high === 89){
+        high = 90; }
+
         if (randomNumbersArray[t] >= low && randomNumbersArray[t] <= high) {
             document.getElementById(`r0c${i}`).value = randomNumbersArray[t];
            
-        }if (high === 89){
-            high = 90;
         }
-      
     }
 
- }if (countEmptyFields1() <= 4) {
+ }if (countEmptyFields() <= 4) {
     loopOnOff = 1;
     
     }
@@ -48,30 +96,107 @@ do {
 }
 
 
-// Counts Empty fields
-function countEmptyFields1() {
-    let emptyFieldCount = 0;
 
-    for (let i = 0; i <= 8; i++) {
-        let elementId = "r0c" + i;
-        let fieldValue = document.getElementById(elementId).value;
 
-        if (fieldValue === "") {
-            emptyFieldCount++;
+
+function bankoRow1() {
+
+    let loopOnOff = 0;
+    
+    do {
+    
+        //Clear all inputs
+        for (let i = 0; i <= 8; i++) {
+            let elementId = "r1c" + i;
+            document.getElementById(elementId).value = "";
         }
+       
+        let randomNumbersArray = [];
+       
+    
+        for (let i = 0; i < 5; i++) {
+            let randomNumber;
+    
+    
+            // udtager random tal og tjekker at alle numre er unikke
+            do {
+                randomNumber = Math.floor(Math.random() * 90) + 1;
+            } while (randomNumbersArray.includes(randomNumber)); //includes er sand hvis random number findes i arrayet
+    
+            randomNumbersArray.push(randomNumber);
+        }
+    
+        console.log(randomNumbersArray);
+    
+        for (let t = 0; t < 5; t++) {
+    
+        for (let i = 0; i < 9; i++) {
+            let low = i * 10;
+            let high = low + 9
+        
+            if (high === 89){
+            high = 90; }
+    
+            if (randomNumbersArray[t] >= low && randomNumbersArray[t] <= high) {
+                document.getElementById(`r1c${i}`).value = randomNumbersArray[t];
+               
+            }
+        }
+    
+     }if (countEmptyFields1() <= 4) {
+        loopOnOff = 1;
+        
+        }
+    } while (loopOnOff === 0);
     }
 
-    return emptyFieldCount;
-}
+    function bankoRow2() {
 
-
-
-
-//Sørg for det ikke er samme tal
-
-//Kontrol 1 af at der ikke kommer 2 i samme felt
-
-
-// Ingen  rows må være tomme
-
-//Hvis der er noget i input field start funktion forfra
+        let loopOnOff = 0;
+        
+        do {
+        
+            //Clear all inputs
+            for (let i = 0; i <= 8; i++) {
+                let elementId = "r2c" + i;
+                document.getElementById(elementId).value = "";
+            }
+           
+            let randomNumbersArray = [];
+           
+        
+            for (let i = 0; i < 5; i++) {
+                let randomNumber;
+        
+        
+                // udtager random tal og tjekker at alle numre er unikke
+                do {
+                    randomNumber = Math.floor(Math.random() * 90) + 1;
+                } while (randomNumbersArray.includes(randomNumber)); //includes er sand hvis random number findes i arrayet
+        
+                randomNumbersArray.push(randomNumber);
+            }
+        
+            console.log(randomNumbersArray);
+        
+            for (let t = 0; t < 5; t++) {
+        
+            for (let i = 0; i < 9; i++) {
+                let low = i * 10;
+                let high = low + 9
+            
+                if (high === 89){
+                high = 90; }
+        
+                if (randomNumbersArray[t] >= low && randomNumbersArray[t] <= high) {
+                    document.getElementById(`r2c${i}`).value = randomNumbersArray[t];
+                   
+                }
+            }
+        
+         }if (countEmptyFields2() <= 4) {
+            loopOnOff = 1;
+            
+            }
+        } while (loopOnOff === 0);
+        }
