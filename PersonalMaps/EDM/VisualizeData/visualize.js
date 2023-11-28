baddfde9b2f2f86958c33489800212e6310eb1fe
EDMerstratec <erme0001@edu.mercantec.dk>
2023-11-28 10:36:39 +0100

    tempPlaying

--- PersonalMaps/EDM/VisualizeData/visualize.js
+++ PersonalMaps/EDM/VisualizeData/visualize.js
@@ -1,3 +1,5 @@
+let KPIparsed;
+
 //object constructor
 function KPI(dateIn, ordersIn, precisionIn, faultsIn) {
     this.dateID = dateIn;
@@ -7,10 +9,10 @@ function KPI(dateIn, ordersIn, precisionIn, faultsIn) {
 }
 
 //use constructor for test values
-const test1 = new KPI(2003, 125, 65.50, 4);
-const test2 = new KPI(2005, 220, 85.50, 2);
-const test3 = new KPI(2004, 330, 75.50, 3);
-let testValueArr = [test1,test2,test3];
+//const test1 = new KPI(2003, 125, 65.50, 4);
+//const test2 = new KPI(2005, 220, 85.50, 2);
+//const test3 = new KPI(2004, 330, 75.50, 3);
+//let testValueArr = [test1,test2,test3];
 
 //sort by orders
 
@@ -25,14 +27,27 @@ function sortBy() {
     }
     console.table(testValueArr);
     //function(a, b), reverse with function(b, a)?
+}
 
+function KPIExcelParse() {
+    let KPIsource = document.getElementById("KPIfile");
+    readXlsxFile(KPIsource.files[0]).then(function(rows) {
+        //rows an array of rows
+        //each row an array of cells
+        KPIparsed = rows;
+    })
+    createObjectKeys();
+    
+}
 
-
+function createObjectKeys() {
+    let numKeys = KPIparsed[0].length;
+    console.log(numKeys)
 }
 
 
 
-let KPIsource = document.getElementById("KPIfile");
+
 
 function calcTable() {
     KPIsource = document.getElementById("KPIfile");
