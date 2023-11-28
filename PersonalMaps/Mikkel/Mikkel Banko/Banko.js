feef71c5e4193ccb284957edee732c8c7339fe4d
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-11-28 09:47:57 +0100

    Control of first row

--- PersonalMaps/Mikkel/Mikkel Banko/Banko.js
+++ PersonalMaps/Mikkel/Mikkel Banko/Banko.js
@@ -2,7 +2,7 @@
 function bankoCard() {
     let randomNumbersArray = [];
 
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 5; i++) {
         let randomNumber;
 
         do {
@@ -32,8 +32,109 @@ function bankoCard() {
         document.getElementById("r0c7").value = randomNumbersArray[0];
     }else if (randomNumbersArray[0] > 79 && randomNumbersArray[0] <= 90) {
         document.getElementById("r0c8").value = randomNumbersArray[0];
-} }
+  
 
+} if (randomNumbersArray[1] < 10) {
+    document.getElementById("r0c0").value = randomNumbersArray[1];
+} else if (randomNumbersArray[1] > 10 && randomNumbersArray[1] <= 19) {
+    document.getElementById("r0c1").value = randomNumbersArray[1];
+} else if (randomNumbersArray[1] > 19 && randomNumbersArray[1] <= 29) {
+    document.getElementById("r0c2").value = randomNumbersArray[1];
+}else if (randomNumbersArray[1] > 29 && randomNumbersArray[1] <= 39) {
+    document.getElementById("r0c3").value = randomNumbersArray[1];
+}else if (randomNumbersArray[1] > 39 && randomNumbersArray[1] <= 49) {
+    document.getElementById("r0c4").value = randomNumbersArray[1];
+}else if (randomNumbersArray[1] > 49 && randomNumbersArray[1] <= 59) {
+    document.getElementById("r0c5").value = randomNumbersArray[1];
+}else if (randomNumbersArray[1] > 59 && randomNumbersArray[1] <= 69) {
+    document.getElementById("r0c6").value = randomNumbersArray[1];
+}else if (randomNumbersArray[1] > 69 && randomNumbersArray[1] <= 79) {
+    document.getElementById("r0c7").value = randomNumbersArray[1];
+}else if (randomNumbersArray[1] > 79 && randomNumbersArray[1] <= 90) {
+    document.getElementById("r0c8").value = randomNumbersArray[1];
+
+}if (randomNumbersArray[2] < 10) {
+        document.getElementById("r0c0").value = randomNumbersArray[2];
+    } else if (randomNumbersArray[2] > 10 && randomNumbersArray[2] <= 19) {
+        document.getElementById("r0c1").value = randomNumbersArray[2];
+    } else if (randomNumbersArray[2] > 19 && randomNumbersArray[2] <= 29) {
+        document.getElementById("r0c2").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[2] > 29 && randomNumbersArray[2] <= 39) {
+        document.getElementById("r0c3").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[2] > 39 && randomNumbersArray[2] <= 49) {
+        document.getElementById("r0c4").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[2] > 49 && randomNumbersArray[2] <= 59) {
+        document.getElementById("r0c5").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[2] > 59 && randomNumbersArray[2] <= 69) {
+        document.getElementById("r0c6").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[2] > 69 && randomNumbersArray[2] <= 79) {
+        document.getElementById("r0c7").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[2] > 79 && randomNumbersArray[2] <= 90) {
+        document.getElementById("r0c8").value = randomNumbersArray[2];
+
+    }if (randomNumbersArray[3] < 10) {
+        document.getElementById("r0c0").value = randomNumbersArray[3];
+    } else if (randomNumbersArray[3] > 10 && randomNumbersArray[3] <= 19) {
+        document.getElementById("r0c1").value = randomNumbersArray[3];
+    } else if (randomNumbersArray[3] > 19 && randomNumbersArray[3] <= 29) {
+        document.getElementById("r0c2").value = randomNumbersArray[3];
+    }else if (randomNumbersArray[3] > 29 && randomNumbersArray[3] <= 39) {
+        document.getElementById("r0c3").value = randomNumbersArray[3];
+    }else if (randomNumbersArray[3] > 39 && randomNumbersArray[3] <= 49) {
+        document.getElementById("r0c4").value = randomNumbersArray[3];
+    }else if (randomNumbersArray[3] > 49 && randomNumbersArray[3] <= 59) {
+        document.getElementById("r0c5").value = randomNumbersArray[3];
+    }else if (randomNumbersArray[3] > 59 && randomNumbersArray[3] <= 69) {
+        document.getElementById("r0c6").value = randomNumbersArray[2];
+    }else if (randomNumbersArray[3] > 69 && randomNumbersArray[3] <= 79) {
+        document.getElementById("r0c7").value = randomNumbersArray[3];
+    }else if (randomNumbersArray[3] > 79 && randomNumbersArray[3] <= 90) {
+        document.getElementById("r0c8").value = randomNumbersArray[3];
+
+    }if (randomNumbersArray[4] < 10) {
+        document.getElementById("r0c0").value = randomNumbersArray[4];
+    } else if (randomNumbersArray[4] > 10 && randomNumbersArray[4] <= 19) {
+        document.getElementById("r0c1").value = randomNumbersArray[4];
+    } else if (randomNumbersArray[4] > 19 && randomNumbersArray[4] <= 29) {
+        document.getElementById("r0c2").value = randomNumbersArray[4];
+    }else if (randomNumbersArray[4] > 29 && randomNumbersArray[4] <= 39) {
+        document.getElementById("r0c3").value = randomNumbersArray[4];
+    }else if (randomNumbersArray[4] > 39 && randomNumbersArray[4] <= 49) {
+        document.getElementById("r0c4").value = randomNumbersArray[4];
+    }else if (randomNumbersArray[4] > 49 && randomNumbersArray[4] <= 59) {
+        document.getElementById("r0c5").value = randomNumbersArray[4];
+    }else if (randomNumbersArray[4] > 59 && randomNumbersArray[4] <= 69) {
+        document.getElementById("r0c6").value = randomNumbersArray[4];
+    }else if (randomNumbersArray[4] > 69 && randomNumbersArray[4] <= 79) {
+        document.getElementById("r0c7").value = randomNumbersArray[4];
+    }else if (randomNumbersArray[4] > 79 && randomNumbersArray[4] <= 90) {
+        document.getElementById("r0c8").value = randomNumbersArray[4];
+
+    }if (countEmptyFields1() > 4) {
+    console.log("Fejl")
+
+}
+
+
+
+
+function countEmptyFields1() {
+    let emptyFieldCount = 0;
+
+    for (let i = 0; i <= 8; i++) {
+        let elementId = "r0c" + i;
+        let fieldValue = document.getElementById(elementId).value;
+
+        if (fieldValue === "") {
+            emptyFieldCount++;
+        }
+    }
+
+    return emptyFieldCount;
+}
+
+
+}
 
 //Sørg for det ikke er samme tal
 
