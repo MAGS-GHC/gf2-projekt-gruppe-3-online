5c8682e741ae62de46e1c166248aa5cac5be6cf4
EDMerstratec <erme0001@edu.mercantec.dk>
2023-11-29 12:54:18 +0100

    MVP
    Now to work on documentation and optionals

--- PersonalMaps/EDM/VisualizeData/visualize.js
+++ PersonalMaps/EDM/VisualizeData/visualize.js
@@ -1,21 +1,6 @@
 let KPIparsed;
 let KPIobject = new Map();
 let keysArray;
-//object constructor
-//function KPI(dateIn, ordersIn, precisionIn, faultsIn) {
-//    this.dateID = dateIn;
-//    this.orders = ordersIn;
-//    this.precision = precisionIn;
-//    this.faults = faultsIn;
-//}
-
-//use constructor for test values
-//const test1 = new KPI(2003, 125, 65.50, 4);
-//const test2 = new KPI(2005, 220, 85.50, 2);
-//const test3 = new KPI(2004, 330, 75.50, 3);
-//let testValueArr = [test1,test2,test3];
-
-//sort by orders
 
 function sortBy() {
     let test = document.getElementById("sortByOptions").value;
@@ -37,8 +22,8 @@ function repopulateTable() {
     console.log(table.rows.length)
     for (row=0;row<(KPIparsed.length);row++) { 
         for (cell=0;cell<keysArray.length;cell++) {
-            table.rows[row+1].cells[cell].innerHTML = KPIparsed[row][cell];
-            //+1row to not overwrite headers
+            table.rows[row+2].cells[cell].innerHTML = KPIparsed[row][cell];
+            //+2row to not overwrite headers
         }
     }
     console.log(table.rows.length)
@@ -76,14 +61,17 @@ function createKPITable() {
     let table = document.getElementById("KPITable");
     table.innerHTML = "";
     let colHead = table.createTHead();
-    let headRow = colHead.insertRow(0)
+    let headRow = colHead.insertRow(0);
+    let meanModeRow = colHead.insertRow(1);
     for (i=0;i<keysArray.length;i++) {
         currentCell = headRow.insertCell(i)
         currentCell.innerHTML = keysArray[i]
+        currentCell = meanModeRow.insertCell(i)
+        currentCell.innerHTML = calcMeanMedian(i);
         console.log("colHead")
     }
     for (i=0;i<KPIparsed.length;i++) {
-        currentRow = table.insertRow(i+1)
+        currentRow = table.insertRow(i+2)
         for (j=0;j<keysArray.length;j++) {
             currentCell = currentRow.insertCell(j);
             currentCell.innerHTML = KPIparsed[i][j];
@@ -92,28 +80,34 @@ function createKPITable() {
 
 }
 
-/*function createObjectKeys() { //does nothing yet, except show top values
 
-    console.log("craaaarrrgh")
-    console.log(keysArray)
-    
-    //KPIobject.set(keysArray); maybe keep for dynamic menu?
-    console.log(KPIobject)
-    console.log(KPIparsed.length)
+function calcMeanMedian(index) {
+    let tempSum = 0;
+    KPIparsed.forEach(element => {
+        tempSum += element[index];
+    });
+    let mean = tempSum / KPIparsed.length;
+    console.log(mean)
 
-    //loop times keyvalues
-    //for (keyNums=0; keyNums<keysArray.length; keyNums++) {
-    //    console.log("Outer loop")
-    //    let tempArray = [];
-    //    KPIparsed.forEach(element => {
-    //        tempArray.push(element[keyNums]);
-    //        //KPIobject.keysArray[keyNums].push(element[keyNums])
-    //    });
-    //    KPIobject.set(keysArray[keyNums], tempArray)
-    //    console.log(tempArray)
-    //}
-    //console.log(KPIobject)
-}*/
+    let tempArray = [];
+    KPIparsed.forEach(element => {
+        tempArray.push(element[index])
+    });
+    tempArray.sort(function(a, b) {return b-a})
+    //tempArray sorted
+    let median;
+    if (tempArray.length % 2 === 0) {
+        median = (tempArray[tempArray.length/2] + tempArray[tempArray.length/2-1]) / 2;
+    }
+    else {
+        median = tempArray[Math.floor(tempArray.length/2)]
+    }
+    console.log(median)
+    console.log(tempArray)
+    console.log((tempArray.length/2))
+    return "Mean: " + Math.round(mean*100)/100 + "\nMedian: " + Math.round(median*100)/100;
+}
+
 
 
 
